2f9dc3aacedbc80a4180df2d87f67ea95d696d0c
ibrahimyryrn <ibrahimyeryaran@gmail.com>
2025-12-09 13:30:07 +0300

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="font-momo text-xl font-bold text-white">
-            ibrahim <span className="text-blue-500">.tech</span>
+            ibrahim Yeryaran
           </a>
 
           <div
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-linear-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I'm İbrahim Tech
+            Hi, I'm İbrahim
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
